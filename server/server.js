36c7b01e2e66496c1552d9100fe5f5cce6359ca0
cartermr <carter.matthew@me.com>
2021-05-05 21:01:58 -0400

    npm init main project add server folder

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+let port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get('/test', (req, res) => {
+    console.log('basic server up running')
+    res.json({response: "basic server up"})
+})
