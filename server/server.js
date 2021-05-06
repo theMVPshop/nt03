a8ef2febb20dc09b4e0d94c0fb1b3f7fbda8ccba
cartermr <carter.matthew@me.com>
2021-05-05 21:08:15 -0400

    quick update to server.js

--- server/server.js
+++ server/server.js
@@ -11,3 +11,5 @@ app.get('/test', (req, res) => {
     console.log('basic server up running')
     res.json({response: "basic server up"})
 })
+
+app.listen(port, () => console.log(`API Server running on port ${port}`))
