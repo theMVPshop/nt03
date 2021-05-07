494cfe7ca8fb149594c36fab50a12313bce401bd
MaryWenzel <wenzel.mary1993@gmail.com>
2021-05-06 20:33:15 -0500

    Added very basic search buttons on landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,8 +4,13 @@ const Landing = () => {
   return (
     <div className="landing-container">
       <div className="main-info">
-        <div>Office Search</div>
-        <div>Job Search</div>
+        <div className="search-button">
+          <a>Office Search</a>
+        </div>
+
+        <div className="search-button">
+          <a>Job Search</a>
+        </div>
       </div>
     </div>
   );
