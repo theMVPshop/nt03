c21f1ffd2fdbc00f572fa57a67f1fbf57b41601c
MaryWenzel <wenzel.mary1993@gmail.com>
2021-05-07 16:43:59 -0500

    Added image files being used for search icons and fixed buttons

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import clinic from '../images/WHITE-clinic-search.png';
+import search from '../images/WHITE-Search.png';
 
 const Landing = () => {
   return (
     <div className="landing-container">
       <div className="main-info">
-        <div className="search-button">
-          <a>Office Search</a>
-        </div>
-
-        <div className="search-button">
-          <a>Job Search</a>
-        </div>
+        <button className="search-button">
+          <Link className="link" to="/clinic-search">
+            Office Search
+          </Link>
+          <img src={clinic} alt="clinic search icon" />
+        </button>
+        <button className="search-button">
+          <Link className="link" to="/job-search">
+            Job Search
+          </Link>
+          <img src={search} alt="job search icon" />
+        </button>
       </div>
     </div>
   );
