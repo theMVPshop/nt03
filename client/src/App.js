66d2c96bff04a0fb4ed41cf0e60c8a6b2f085957
MaryWenzel <wenzel.mary1993@gmail.com>
2021-05-07 19:35:53 -0500

    Saved changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import '../src/css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
 import Router from './Router';
-=======
 import './css/App.css';
->>>>>>> origin/search-pages
 
 function App() {
   return (
