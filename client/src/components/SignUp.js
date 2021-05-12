03d019b16e9070aa4e272d8d9239d4f094e7b19f
MaryWenzel <wenzel.mary1993@gmail.com>
2021-05-11 19:28:11 -0500

    Changed all uses of 'class' to 'className'

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,40 +3,40 @@ import React from 'react';
 const SignUp = () => {
   return (
     <div className='sign-in-container'>
-      <div class='login-form'>
+      <div className='login-form'>
         <form action='/examples/actions/confirmation.php' method='post'>
-          <h2 class='text-center'>Sign Up</h2>
-          <div class='form-group'>
+          <h2 className='text-center'>Sign Up</h2>
+          <div className='form-group'>
             <input
               type='text'
-              class='form-control'
+              className='form-control'
               placeholder='Enter Email'
               required='required'
             />
           </div>
-          <div class='form-group'>
+          <div className='form-group'>
             <input
               type='password'
-              class='form-control'
+              className='form-control'
               placeholder='Password'
               required='required'
             />
           </div>
-          <div class='form-group'>
+          <div className='form-group'>
             <input
               type='password'
-              class='form-control'
+              className='form-control'
               placeholder='Re-Enter Password'
               required='required'
             />
           </div>
-          <div class='form-group button-box'>
-            <button type='submit' class='btn btn-primary btn-block'>
+          <div className='form-group button-box'>
+            <button type='submit' className='btn btn-primary btn-block'>
               Sign Up
             </button>
           </div>
         </form>
-        <p class='text-center'>
+        <p className='text-center'>
           <a href='/sign-in'>Already have an account? Sign In here.</a>
         </p>
       </div>
