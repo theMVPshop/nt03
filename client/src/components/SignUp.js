6cdc2ee77901c3c0b628506d4bbfa35810b3d4e6
MaryWenzel <wenzel.mary1993@gmail.com>
2021-05-11 19:24:22 -0500

    Fixed centering on Sign In page

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,42 +2,44 @@ import React from 'react';
 
 const SignUp = () => {
   return (
-    <div class='login-form'>
-      <form action='/examples/actions/confirmation.php' method='post'>
-        <h2 class='text-center'>Sign Up</h2>
-        <div class='form-group'>
-          <input
-            type='text'
-            class='form-control'
-            placeholder='Enter Email'
-            required='required'
-          />
-        </div>
-        <div class='form-group'>
-          <input
-            type='password'
-            class='form-control'
-            placeholder='Password'
-            required='required'
-          />
-        </div>
-        <div class='form-group'>
-          <input
-            type='password'
-            class='form-control'
-            placeholder='Re-Enter Password'
-            required='required'
-          />
-        </div>
-        <div class='form-group button-box'>
-          <button type='submit' class='btn btn-primary btn-block'>
-            Sign Up
-          </button>
-        </div>
-      </form>
-      <p class='text-center'>
-        <a href='/sign-in'>Already have an account? Sign In here.</a>
-      </p>
+    <div className='sign-in-container'>
+      <div class='login-form'>
+        <form action='/examples/actions/confirmation.php' method='post'>
+          <h2 class='text-center'>Sign Up</h2>
+          <div class='form-group'>
+            <input
+              type='text'
+              class='form-control'
+              placeholder='Enter Email'
+              required='required'
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='password'
+              class='form-control'
+              placeholder='Password'
+              required='required'
+            />
+          </div>
+          <div class='form-group'>
+            <input
+              type='password'
+              class='form-control'
+              placeholder='Re-Enter Password'
+              required='required'
+            />
+          </div>
+          <div class='form-group button-box'>
+            <button type='submit' class='btn btn-primary btn-block'>
+              Sign Up
+            </button>
+          </div>
+        </form>
+        <p class='text-center'>
+          <a href='/sign-in'>Already have an account? Sign In here.</a>
+        </p>
+      </div>
     </div>
   );
 };
