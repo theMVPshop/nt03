2aa3e5b19619341b0a672b63aa1ad3e10fba6bc2
MaryWenzel <wenzel.mary1993@gmail.com>
2021-05-12 20:16:47 -0500

    Fixed Merge issue

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -8,17 +8,6 @@ const SearchForm = (props) => {
 
   return (
     <div className='search-form-container'>
-<<<<<<< HEAD
-      <img className='search-form-icon' src={props.icon} alt={props.alt} />
-      <form className='search-form' onSubmit={handleSubmit}>
-        <input
-          className='search-input'
-          type='text'
-          placeholder={props.placeholder1}
-          onChange={handleTextChange}
-        />
-        {props.placeholder2 === undefined ? null : (
-=======
       <div className='search-form'>
         <img className='search-form-icon' src={props.icon} alt={props.alt} />
         <form className='search-form' onSubmit={handleSubmit}>
@@ -28,26 +17,19 @@ const SearchForm = (props) => {
             placeholder={props.placeholder1}
             onChange={handleTextChange}
           />
-          {props.placeholder2 === undefined ? null :
->>>>>>> b1a7d4758f6e8f639002654cc267cb15d03c355b
-          <input
-            className='search-input'
-            type='text'
-            placeholder={props.placeholder2}
-            onChange={handleTextChange}
-          />
-<<<<<<< HEAD
-        )}
-        <button className='button form-button' type='submit'>
-          SEARCH
-        </button>
-      </form>
-=======
-          }
-          <button className='btn form-btn' type='submit'>SEARCH</button>
+          {props.placeholder2 === undefined ? null : (
+            <input
+              className='search-input'
+              type='text'
+              placeholder={props.placeholder2}
+              onChange={handleTextChange}
+            />
+          )}
+          <button className='btn form-btn' type='submit'>
+            SEARCH
+          </button>
         </form>
       </div>
->>>>>>> b1a7d4758f6e8f639002654cc267cb15d03c355b
     </div>
   );
 };
