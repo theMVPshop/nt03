0add0ca6002a9d3142453e46e20310c69abe2859
Shelby-Jean <shelby.gottschall@gmail.com>
2021-05-13 20:33:14 -0400

    sm search component styling

--- client/src/components/SearchFormSm.js
+++ client/src/components/SearchFormSm.js
@@ -9,27 +9,25 @@ const SearchFormSm = (props) => {
 
   return (
     <div className='sm-search-form-container'>
-      <div className='sm-search-form'>
-        <form className='sm-search-form' onSubmit={handleSubmit}>
+      <form className='sm-search-form' onSubmit={handleSubmit}>
+        <input
+          className='sm-search-input'
+          type='text'
+          placeholder={props.placeholder1}
+          onChange={handleTextChange}
+        />
+        {props.placeholder2 === undefined ? null : (
           <input
             className='sm-search-input'
             type='text'
-            placeholder={props.placeholder1}
+            placeholder={props.placeholder2}
             onChange={handleTextChange}
           />
-          {props.placeholder2 === undefined ? null : (
-            <input
-              className='sm-search-input'
-              type='text'
-              placeholder={props.placeholder2}
-              onChange={handleTextChange}
-            />
-          )}
-          <button className='btn form-btn' type='submit'>
-            SEARCH
-          </button>
-        </form>
-      </div>
+        )}
+        <button className='btn sm-btn' type='submit'>
+          SEARCH
+        </button>
+      </form>
     </div>
   );
 };
