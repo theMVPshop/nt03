9e52d05c6ef8d9b070aa6c9ee46beee802f71428
JEFFGULICK <jeffgulick@gmail.com>
2021-05-17 21:45:56 -0500

    added auth endpoints, need to test, do not merge

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 // Routers endpoints
 app.use(usersRoute);
 app.use(jobsRouter);
+app.use(require('./Routes/auth'));
 
 app.get('/test', (req, res) => {
   console.log('basic server up running');
