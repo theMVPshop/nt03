97c2d1c59233bfd9291d3e0e3acd0b4ec3e991ec
Shelby-Jean <shelby.gottschall@gmail.com>
2021-05-17 20:32:02 -0400

    news carousel stlying

--- client/src/components/NewsArticles.js
+++ client/src/components/NewsArticles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import dentalOffice from '../images/dental-office.jpeg';
 
 // id='divRss'
@@ -7,32 +8,34 @@ import dentalOffice from '../images/dental-office.jpeg';
 const NewsArticles = (props) => {
   
   return (
-    <div className='news-container'>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={dentalOffice}
-            alt="dental office"
-          />
-          <Carousel.Caption>
-            <h3>News Headline</h3>
-            <p>News article descrption...</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={dentalOffice}
-            alt="dental office"
-          />
-          <Carousel.Caption>
-            <h3>Another News Headline</h3>
-            <p>Another news article descrption...</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
+    <a href='https://newsarticlelink.com' target='blank'>
+      <div className='news-container'>
+        <Carousel prevLabel={BsChevronLeft} nextLabel={BsChevronRight} interval={null}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={dentalOffice}
+              alt="dental office"
+            />
+            <Carousel.Caption>
+              <h3>News Headline</h3>
+              <p>News article descrption...</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={dentalOffice}
+              alt="dental office"
+            />
+            <Carousel.Caption>
+              <h3>Another News Headline</h3>
+              <p>Another news article descrption...</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </a>
   )
 
 }
