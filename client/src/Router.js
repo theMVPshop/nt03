8bdc358276daf39d3a4575ff23cf9748e171d28e
cartermr <carter.matthew@me.com>
2021-05-17 20:54:32 -0400

    update map-integration

--- client/src/Router.js
+++ client/src/Router.js
@@ -6,6 +6,7 @@ import ClinicSearch from '../src/components/ClinicSearch';
 import SignIn from '../src/components/SignIn';
 import SignUp from '../src/components/SignUp';
 import AddressBook from './components/AddressBook';
+import ClinicSearchResults from './components/ClinicSearchResults'
 
 function Router() {
   const [clinicSearch, setClinicSearch] = useState("")
@@ -14,7 +15,12 @@ function Router() {
     <Switch>
       <Route exact path='/' component={Landing}></Route>
       <Route path='/job-search' component={JobSearch}></Route>
-      <Route path='/clinic-search' component={ClinicSearch}></Route>
+      <Route path='/clinic-search'>
+        <ClinicSearch setClinicSearch={setClinicSearch} />
+      </Route>
+      <Route path='/clinic-results'>
+        <ClinicSearchResults clinicSearch={clinicSearch} />
+      </Route>
       <Route path='/sign-in' component={SignIn}></Route>
       <Route path='/sign-up' component={SignUp}></Route>
       <Route path='/address-book' component={AddressBook}></Route>
