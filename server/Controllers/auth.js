39160e6b3da8da01523d3faa7f9647431417221b
JEFFGULICK <jeffgulick@gmail.com>
2021-05-19 21:17:23 -0500

    configured access token request, password hashing, and auth

--- server/Controllers/auth.js
+++ server/Controllers/auth.js
@@ -7,30 +7,10 @@ const jwt = require('jsonwebtoken');
 const pool = require('../database/dbConnection');
 const { handleSQLError } = require('../database/error');
 
-// for bcrypt
-const saltRounds = 10;
-
-const signup = (req, res) => {
-  const { username, password } = req.body;
-  let sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
-
-  bcrypt.hash(password, saltRounds, async (err, hash) => {
-    sql = mysql.format(sql, [username, hash]);
-
-    await pool.query(sql, (err, result) => {
-      if (err) {
-        if (err.code === 'ER_DUP_ENTRY')
-          return res.status(409).send('Username is taken');
-        return handleSQLError(res, err);
-      }
-      return res.send('Sign-up successful');
-    });
-  });
-};
-
 const login = (req, res) => {
   const { username, password } = req.body;
 
+  //requesting access token
   axios(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
     method: 'POST',
     headers: {
@@ -56,30 +36,29 @@ const login = (req, res) => {
       res.send(e);
     });
 
-  // let sql = 'SELECT * FROM users WHERE username = ?';
-  // sql = mysql.format(sql, [username]);
+  let sql = 'SELECT * FROM users WHERE username = ?';
+  sql = mysql.format(sql, [username]);
 
-  // pool.query(sql, (err, rows) => {
-  //   if (err) return handleSQLError(res, err);
-  //   if (!rows.length) return res.status(404).send('No matching users');
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    if (!rows.length) return res.status(404).send('No matching users');
 
-  //   const hash = rows[0].password;
-  //   bcrypt.compare(password, hash).then((result) => {
-  //     if (!result) return res.status(400).send('Invalid password');
+    const hash = rows[0].password;
+    bcrypt.compare(password, hash).then((result) => {
+      if (!result) return res.status(400).send('Invalid password');
 
-  //     const data = { ...rows[0] };
-  //     data.password = 'REDACTED';
+      const data = { ...rows[0] };
+      data.password = 'REDACTED';
 
-  //     const token = jwt.sign(data, 'secret');
-  //     res.json({
-  //       msg: 'Login successful',
-  //       token,
-  //     });
-  //   });
-  // });
+      const token = jwt.sign(data, 'secret');
+      res.json({
+        msg: 'Login successful',
+        token,
+      });
+    });
+  });
 };
 
 module.exports = {
-  signup,
   login,
 };
