b850e32c6e4417430305bbacf21e31674588cdc1
Shelby-Jean <shelby.gottschall@gmail.com>
2021-05-20 20:37:08 -0400

    news article content displaying

--- client/src/components/NewsArticles.js
+++ client/src/components/NewsArticles.js
@@ -3,140 +3,138 @@ import { Carousel } from 'react-bootstrap';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import dentalOffice from '../images/dental-office.jpeg';
 
-// id='divRss'
-
 const NewsArticles = (props) => {
   
   return (
     <div className='carousel-container'>
       <Carousel prevLabel={BsChevronLeft} nextLabel={BsChevronRight} interval={null} indicators={false}>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 1</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 1</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 2</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 2</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 3</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 3</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 4</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 4</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 5</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 5</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 6</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 6</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 7</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 7</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 8</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 8</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 9</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 9</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
         <Carousel.Item>
-          <a href='https://newsarticlelink.com' target='blank'>
+          <a className='news-link' href='https://newsarticlelink.com' target='blank'>
             <img
               className="d-block w-100"
               src={dentalOffice}
               alt="dental office"
             />
             <Carousel.Caption>
-              <h3>News Headline 10</h3>
-              <p>News article descrption...</p>
+              <h3 className='news-title'>News Headline 10</h3>
+              <p className='news-description'>News article descrption...</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
