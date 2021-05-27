1966bfe072779011779d03da27d2f56b97637632
Shelby-Jean <shelby.gottschall@gmail.com>
2021-05-26 21:02:59 -0400

    job postings data coming in

--- client/src/components/JobPostings.js
+++ client/src/components/JobPostings.js
@@ -1,39 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import SearchFormSm from './SearchFormSm';
 import JobCard from './JobCard';
 import NewsArticles from './NewsArticles';
 import FlashCards from './FlashCards';
 
 const JobPostings = ({jobSearch}) => {
-  console.log(jobSearch);
   const [jobList, setJobList] = useState([]);
-  // const [selectedJob, setSelectedJob] = useState({});
 
   useEffect(() => {
-    const url = "https://jooble.org/api/";
-    const key = "2f68c697-0b9e-420b-ac07-3522403e50ae";
-    let params = { "keywords": jobSearch.position, "location": jobSearch.location };
-
-    //create xmlHttpRequest object
-    const http = new XMLHttpRequest();
-    //open connection. true - asynchronous, false - synchronous
-    http.open("POST", url + key, true);
-
-    //Send the proper header information
-    http.setRequestHeader("Content-type", "application/json");
-      
-    //Callback when the state changes
-    http.onreadystatechange = function() {
-      if(http.readyState == 4 && http.status == 200) {
-        let data = JSON.parse(http.responseText);
+    axios.post('https://jooble.org/api/2f68c697-0b9e-420b-ac07-3522403e50ae', {
+      keywords: jobSearch.position,
+      location: jobSearch.location
+    })
+      .then(res => res.data)
+      .then(data => {
         let jobs = data.jobs;
         setJobList(jobs);
-        // setSelectedJob(data[0])
-      }
-    }
-    //Send request to the server
-    http.send(params);
-  }, [])
+        console.log(jobList)
+      })
+  }, []);
   
   return (
     <div>
@@ -45,8 +31,16 @@ const JobPostings = ({jobSearch}) => {
             <JobCard />
             <JobCard />
             <JobCard />
-            <JobCard />
-            <JobCard />
+            {/* {jobList.map((job) => {
+              <JobCard 
+                key={job.id}
+                title={job.title}
+                location={job.location}
+                company={job.company}
+                description={job.snippet}
+                link={job.link}
+              />
+            })} */}
           </div>
           {/* <div className='news-grid'> */}
             <NewsArticles />
@@ -71,3 +65,32 @@ export default JobPostings;
 //     link={job.link}
 //   />
 // })}
+
+
+// const url = "https://jooble.org/api/";
+//     const key = "2f68c697-0b9e-420b-ac07-3522403e50ae";
+//     // let params = "{ keywords: 'dentist', location: 'austin'}";
+//     // let params = `{ keywords: ${jobSearch.position}, location: ${jobSearch.location}}`;
+//     // let params = { "keywords": jobSearch.position, "location": jobSearch.location };
+//     // console.log(params);
+
+//     //create xmlHttpRequest object
+//     const http = new XMLHttpRequest();
+//     //open connection. true - asynchronous, false - synchronous
+//     http.open("POST", url + key, true);
+
+//     //Send the proper header information
+//     http.setRequestHeader("Content-type", "application/json");
+      
+//     //Callback when the state changes
+//     http.onreadystatechange = function() {
+//       if(http.readyState == 4 && http.status == 200) {
+//         let data = JSON.parse(http.responseText);
+//         let jobs = data.jobs;
+//         // console.log(jobs);
+//         // setJobList(jobs);
+//         // setSelectedJob(data[0])
+//       }
+//     }
+//     //Send request to the server
+//     http.send(params);
