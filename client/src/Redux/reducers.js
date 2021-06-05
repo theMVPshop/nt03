47ae42f70c69f5ea39850e3a0a0cdeec5bbb1c6a
JEFFGULICK <jeffgulick@gmail.com>
2021-06-04 20:02:15 -0500

    added login status to redux

--- client/src/Redux/reducers.js
+++ client/src/Redux/reducers.js
@@ -11,7 +11,18 @@ const user = (state = {}, action) => {
       return state;
   }
 };
+const signInStatus = (state = false, action) => {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return true;
+    case 'SIGN_OUT':
+      return false;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
   user,
+  signInStatus,
 });
