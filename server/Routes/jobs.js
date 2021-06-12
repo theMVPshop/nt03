e1619205ac3cad79fa0d3b60f1b0d7a8934ac7db
Matt Carter <carter.matthew@icloud.com>
2021-06-11 21:32:04 -0400

    adjusting saved jobs endpoints

--- server/Routes/jobs.js
+++ server/Routes/jobs.js
@@ -9,12 +9,15 @@ const jobs = require('../Controllers/jobs');
 router.get('/jobs', jobs.allJobs);
 
 // List of saved jobs by user
-router.get('/jobs/:id', jobs.jobByID);
+router.get('/jobs/:userID', jobs.jobByID);
 
 // Save new job to user
 router.post('/jobs', jobs.saveJob);
 
 // Update job
 router.put('/jobs', jobs.updateJob);
+
+// delete job
+router.delete('/jobs/:jobID', jobs.deleteJob);
 
 module.exports = router;
